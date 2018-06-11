1fdbf6f656eb2906f40ca12c569beb086f314604
Houssem Zitoun <houssemzitoun91@gmail.com>
2018-06-11 17:19:41 +0100

    Definition of namespaces

--- constant-ns.php
+++ constant-ns.php
@@ -5,6 +5,6 @@ namespace NS;
 define('C', "I am a constant");
 const A = "I am a letter";
 
-echo __NAMESPACE__;
+echo __NAMESPACE__, PHP_EOL;
 echo namespace\A, PHP_EOL;
 echo namespace\C, PHP_EOL;
